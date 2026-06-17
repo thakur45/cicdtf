2ddee270030fd6539c18d2c0e6705f6106758dd6
Mayuresh Thakur <mayuresh.thakur@deltatre.com>
2026-06-17 11:16:09 +0530

    change1

--- web/main.tf
+++ web/main.tf
@@ -1,5 +1,4 @@
 resource "aws_instance" "web" {
-  count                   = 2
   ami                    = "ami-0f58b397bc5c1f2e8"
   instance_type          = "t2.micro"
   subnet_id              = var.subnet_id
